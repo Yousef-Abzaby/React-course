7cd0c2c74b44208295e278608a315aeaa19541ec
Yousef-Mohammed <yousef.abzaby@gmail.com>
2025-10-26 14:24:37 +0300

    added checkout page to react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,15 @@
 import { Homepage } from "./Pages/Homepage.jsx";
+import { CheckoutPage } from "./Pages/CheckoutPage.jsx";
+import { Routes, Route } from "react-router";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Homepage />
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+      </Routes>
     </>
   );
 }
