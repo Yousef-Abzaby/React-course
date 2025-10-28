62ed016a3d076d301ae1f11d9dbf617d269f06c1
Yousef-Mohammed <yousef.abzaby@gmail.com>
2025-10-28 12:17:44 +0300

    fetching data from backend

--- ecommerce-project/src/Pages/Homepage.jsx
+++ ecommerce-project/src/Pages/Homepage.jsx
@@ -4,6 +4,13 @@ import { products } from "../../starting-code/data/products.js";
 import { Header } from "../components/Header.jsx";
 
 export function Homepage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
